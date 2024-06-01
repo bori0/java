dcbb329d7382944f345c2f22945a2cdeb4ec3844
bori <156080750+bori0@users.noreply.github.com>
2024-06-01 15:21:51 +0900

    자바 firstexam

---
+++ firstExam/src/initExam/Exam.java
@@ -0,0 +1,14 @@
+package initExam;
+
+public class Exam {//class의 시작
+
+	public static void main(String[] args) { //메소드의 시작
+		// TODO Auto-generated method stub
+		System.out.println("Hello!! java!!");
+		System.out.println("즐거운");
+		System.out.println("자바");
+		System.out.println("3");
+
+	}//메소드의 끝
+
+}//class의 끝
